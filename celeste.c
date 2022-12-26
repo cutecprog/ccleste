af1b4db8d8a10f14c9beb831c6ea7cb9bd66e9a6
cutecprog <freefowl@gmx.com>
2022-12-26 04:13:45 -0500

    Minor changes to title screen

--- celeste.c
+++ celeste.c
@@ -1856,8 +1856,8 @@ void Celeste_P8_draw() {
    
 	// credits
 	if (is_title()) {
-		P8print("x+c",58,80,5);
-		P8print("maddy thorson",42,96,5);
+		P8print("jump+dash",47,80,5);
+		P8print("maddy thorson",41,96,5);
 		P8print("noel berry",46,102,5);
 	}
    
